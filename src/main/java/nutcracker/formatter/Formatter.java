78ca6fa96f76162bb464a33c6baa3eb03badc451
Markowski <wmarkowski@psi.de>
2017-06-07 12:42:08 +0100

    Read data from application input stream and print the to output stream.

--- src/main/java/nutcracker/formatter/Formatter.java
+++ src/main/java/nutcracker/formatter/Formatter.java
@@ -1,5 +1,11 @@
 package nutcracker.formatter;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -8,6 +14,33 @@ public class Formatter {
 	private static final Logger LOGGER = LoggerFactory.getLogger(Formatter.class);
 
 	public static void main(String[] args) {
-		LOGGER.info("Hello world!");
+		LOGGER.info("Application started with args: ");
+		for (String arg : args) {
+			LOGGER.info(arg);
+		}
+
+		readInputFile(System.in);
+
+		LOGGER.info("Application finished");
+	}
+
+	private static List<String> readInputFile(InputStream is) {
+		long lastReadTime = System.currentTimeMillis();
+		long timeout = 1000;
+		BufferedReader bsr = new BufferedReader(new InputStreamReader(is));
+
+		try {
+			while (System.currentTimeMillis() - lastReadTime < timeout) {
+				if (bsr.ready()) {
+					String line = bsr.readLine();
+					lastReadTime = System.currentTimeMillis();
+					System.out.println(line);
+				}
+			}
+		} catch (IOException e) {
+			LOGGER.error(e.getMessage(), e);
+		}
+
+		return null;
 	}
 }
